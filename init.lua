1d4b002b11fbd802a4539e8e58ec2f22081d6349
Arcelmi <Arcelmi@users.noreply.github.com>
2017-09-14 16:01:06 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ if minetest.settings:get("movement_gravity") then
 end
 local charge_speed = 1
 if minetest.settings:get("bow_full_charge_time") then
-	charge_speed = tonumber(minetest.settings:get("bow_full_charge_time"))/3
+	charge_speed = tonumber(minetest.settings:get("bow_full_charge_time"))/2
 end
 local arrow_lifetime = 60
 if minetest.settings:get("bow_lifetime_arrow") then
@@ -76,9 +76,10 @@ minetest.register_craftitem("bow:arrow", {
 })
 
 minetest.register_entity("bow:arrow_ent", {
-	physical = false,
+	physical = true,
 	visual = "mesh",
 	mesh = "arrow.obj",
+	collide_with_objects = false,
 	visual_size = {x=1, y=1},
     collisionbox = {-0.1,-0.1,-0.1, 0.1,0.1,0.1},
 	textures = {"bow_arrow_uv.png"},
@@ -86,7 +87,8 @@ minetest.register_entity("bow:arrow_ent", {
 		if not self.start_timer then self.object:remove() return end
 		if not self.charge then self.object:remove() return end
 		local pos = self.object:getpos()
-		if minetest.registered_nodes[minetest.get_node(pos).name] and minetest.registered_nodes[minetest.get_node(pos).name].walkable and minetest.registered_nodes[minetest.get_node(pos).name].drawtype~="nodebox" and self.charge>0 then
+		local vel = self.object:getvelocity()
+		if ((self.start_vel.x~=0 and vel.x==0) or (self.start_vel.z~=0 and vel.z==0) or vel.y==0) and self.charge>0 then
 			self.object:setvelocity({x=0, y=0, z=0})
 			self.object:setacceleration({x=0, y=0, z=0})
 			self.charge = 0
@@ -122,6 +124,56 @@ minetest.register_entity("bow:arrow_ent", {
 local timer = 0
 local bow_load={}
 
+controls.register_on_release(function(player, key, time)
+	if key~="RMB" then return end
+	local inv = minetest.get_inventory({type="player", name=player:get_player_name()})
+	local wielditem = player:get_wielded_item()
+	if (wielditem:get_name()=="bow:bow_1" or wielditem:get_name()=="bow:bow_2" or wielditem:get_name()=="bow:bow_3") then
+		local yaw = player:get_look_yaw()
+		local dir = player:get_look_dir()
+		local pos = vector.add(player:getpos(),{x=dir.x*2, y=dir.y*2+1.5, z=dir.z*2})
+		local obj = minetest.add_entity(pos, "bow:arrow_ent")
+		obj:setyaw(yaw + math.pi)
+		local charge = 1
+		if wielditem:get_name()=="bow:bow_1" then
+			charge = 1
+		elseif wielditem:get_name()=="bow:bow_2" then
+			charge = 2
+		elseif wielditem:get_name()=="bow:bow_3" then
+			charge = 3
+		end
+		obj:setvelocity({x=dir.x*charge*18,055566667, y=dir.y*charge*18,055566667, z=dir.z*charge*18,055566667})
+		obj:setacceleration({x=0, y=gravity, z=0})
+		obj:get_luaentity().charge = charge
+		obj:get_luaentity().player = player
+		obj:get_luaentity().start_timer = 0
+		obj:get_luaentity().start_vel = {x=dir.x*charge*18,055566667, y=dir.y*charge*18,055566667, z=dir.z*charge*18,055566667}
+		player:set_physics_override({jump=1, gravity=1, speed=1})
+		wielditem:set_name("bow:bow")
+		wielditem:add_wear(charge*100)
+		player:set_wielded_item(wielditem)
+		inv:remove_item("main", "bow:arrow 1")
+		bow_load[player:get_player_name()]=false
+		player:set_eye_offset({x=0,y=0,z=0},{x=0,y=0,z=0})
+	end
+end)
+
+controls.register_on_hold(function(player, key, time)
+	if key~="RMB" then return end
+	local inv = minetest.get_inventory({type="player", name=player:get_player_name()})
+	local wielditem = player:get_wielded_item()
+	if wielditem:get_name()=="bow:bow" and inv:contains_item("main", "bow:arrow") then
+		player:set_physics_override({jump=0.5, gravity=0.25, speed=0.25})
+		wielditem:set_name("bow:bow_1")
+		bow_load[player:get_player_name()]=os.clock()
+	elseif wielditem:get_name()=="bow:bow_1" and os.clock()-bow_load[player:get_player_name()]>charge_speed then
+		wielditem:set_name("bow:bow_2")
+	elseif wielditem:get_name()=="bow:bow_2" and os.clock()-bow_load[player:get_player_name()]>charge_speed*2 then
+		wielditem:set_name("bow:bow_3")
+	end
+	player:set_wielded_item(wielditem)
+end)
+
 minetest.register_globalstep(function(dtime)
 	for _, player in pairs(minetest.get_connected_players()) do
 		local wielditem = player:get_wielded_item()
@@ -132,33 +184,7 @@ minetest.register_globalstep(function(dtime)
 		local controls = player:get_player_control()
 		timer = timer+dtime
 		local inv = minetest.get_inventory({type="player", name=player:get_player_name()})
-		if (wielditem:get_name()=="bow:bow_1" or wielditem:get_name()=="bow:bow_2" or wielditem:get_name()=="bow:bow_3") and not controls.RMB then
-			local yaw = player:get_look_yaw()
-			local dir = player:get_look_dir()
-			local pos = vector.add(player:getpos(),{x=dir.x*2, y=dir.y*2+1.5, z=dir.z*2})
-			local obj = minetest.add_entity(pos, "bow:arrow_ent")
-			obj:setyaw(yaw + math.pi)
-			local charge = 1
-			if wielditem:get_name()=="bow:bow_1" then
-				charge = 1
-			elseif wielditem:get_name()=="bow:bow_2" then
-				charge = 2
-			elseif wielditem:get_name()=="bow:bow_3" then
-				charge = 3
-			end
-			obj:setvelocity({x=dir.x*charge*18,055566667, y=dir.y*charge*18,055566667, z=dir.z*charge*18,055566667})
-			obj:setacceleration({x=0, y=gravity, z=0})
-			obj:get_luaentity().charge = charge
-			obj:get_luaentity().player = player
-			obj:get_luaentity().start_timer = 0
-			player:set_physics_override({jump=1, gravity=1, speed=1})
-			wielditem:set_name("bow:bow")
-			wielditem:add_wear(charge*100)
-			player:set_wielded_item(wielditem)
-			inv:remove_item("main", "bow:arrow 1")
-			bow_load[player:get_player_name()]=false
-		end
-		if bow_load[player:get_player_name()]==true and (wielditem:get_name()~="bow:bow_1" and wielditem:get_name()~="bow:bow_2" and wielditem:get_name()~="bow:bow_3") then
+		if bow_load[player:get_player_name()] and (wielditem:get_name()~="bow:bow_1" and wielditem:get_name()~="bow:bow_2" and wielditem:get_name()~="bow:bow_3") then
 			local list = inv:get_list("main")
 			for place, stack in pairs(list) do
 				if stack:get_name()=="bow:bow_1" or stack:get_name()=="bow:bow_2" or stack:get_name()=="bow:bow_3" then
@@ -169,21 +195,8 @@ minetest.register_globalstep(function(dtime)
 			end
 			inv:set_list("main", list)
 			player:set_physics_override({jump=1, gravity=1, speed=1})
+			player:set_eye_offset({x=0,y=0,z=0},{x=0,y=0,z=0})
 			bow_load[player:get_player_name()]=false
-		end 
-		if timer>=charge_speed then
-			wielditem = player:get_wielded_item()
-			timer=0
-			if wielditem:get_name()=="bow:bow" and controls.RMB and inv:contains_item("main", "bow:arrow") then
-				player:set_physics_override({jump=0.5, gravity=0.25, speed=0.25})
-				wielditem:set_name("bow:bow_1")
-				bow_load[player:get_player_name()]=true
-			elseif wielditem:get_name()=="bow:bow_1" and controls.RMB then
-				wielditem:set_name("bow:bow_2")
-			elseif wielditem:get_name()=="bow:bow_2" and controls.RMB then
-				wielditem:set_name("bow:bow_3")
-			end
-			player:set_wielded_item(wielditem)
 		end
 	end
 end)
